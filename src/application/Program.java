963a2f74325c91ae28ecfcf155a5fefd5add42af
Maycon Felipe <mayconfelipe164@gmail.com>
2020-11-19 12:28:09 -0300

    Create SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "boks");
 		Seller seller = new Seller(21, "Maycon", "mayconn@gmail.com", new Date(), 3.000, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj);
 		System.out.println(seller);
