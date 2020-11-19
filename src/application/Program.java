a7401d23a60b37d3ba2eeeebefdf7609e7e23021
Maycon Felipe <mayconfelipe164@gmail.com>
2020-11-19 10:51:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "boks");
+		Seller seller = new Seller(21, "Maycon", "mayconn@gmail.com", new Date(), 3.000, obj);
+		
 		System.out.println(obj);
-
+		System.out.println(seller);
 	}
 
 }
