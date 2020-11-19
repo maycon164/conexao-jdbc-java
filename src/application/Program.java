d1ea6b1ecd7d7e4d2237e624fd13052dc547cc8b
Maycon Felipe <mayconfelipe164@gmail.com>
2020-11-19 12:52:08 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,15 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "boks");
-		Seller seller = new Seller(21, "Maycon", "mayconn@gmail.com", new Date(), 3.000, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+	
 		System.out.println(seller);
 	}
-
 }
